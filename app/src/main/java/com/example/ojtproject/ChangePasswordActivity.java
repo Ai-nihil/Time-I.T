d94eb44e4c81124fba392304fcaf676cdc91a3cc
JborJ46 <borjy46@gmail.com>
2023-09-11 23:18:17 +0800

    Added changes won't be saved dialog on some activities that edit data

--- app/src/main/java/com/example/ojtproject/ChangePasswordActivity.java
+++ app/src/main/java/com/example/ojtproject/ChangePasswordActivity.java
@@ -1,6 +1,7 @@
 package com.example.ojtproject;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -8,7 +9,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,9 +35,12 @@ public class ChangePasswordActivity extends AppCompatActivity {
                      changePasswordActivityEditTextNewPassword,
                      changePasswordActivityEditTextConfirmPassword;
     private Button changePasswordActivityButtonAuthenticate,
-                   changePasswordActivityButtonConfirmPassword;
+                   changePasswordActivityButtonConfirmPassword,
+                   changePasswordActivityButtonCancel;
     private TextView changePasswordActivityTextViewUpdateEmailAuthenticated;
     private String userCurrentPassword;
+    private Boolean changesMadeCurrentPassword = false,
+                    changesMadeAuthentication = false;
     private ProgressBar changePasswordActivityProgressBar;
 
     @Override
@@ -42,11 +48,39 @@ public class ChangePasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         changePasswordActivityEditTextCurrentPassword = findViewById(R.id.changePasswordActivityEditTextCurrentPassword);
+        changePasswordActivityEditTextCurrentPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                if (!charSequence.toString().isEmpty()) {
+                    changesMadeCurrentPassword = true;
+                } else {
+                    changesMadeCurrentPassword = false;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         changePasswordActivityEditTextNewPassword = findViewById(R.id.changePasswordActivityEditTextNewPassword);
         changePasswordActivityEditTextConfirmPassword = findViewById(R.id.changePasswordActivityEditTextConfirmPassword);
         changePasswordActivityButtonAuthenticate = findViewById(R.id.changePasswordActivityButtonAuthenticate);
+        if(!changePasswordActivityButtonAuthenticate.isEnabled()){
+            changesMadeAuthentication = true;
+        } else {
+            changesMadeAuthentication = false;
+        }
         changePasswordActivityButtonConfirmPassword = findViewById(R.id.changePasswordActivityButtonConfirmPassword);
+        changePasswordActivityButtonCancel = findViewById(R.id.changePasswordActivityButtonCancel);
         changePasswordActivityTextViewUpdateEmailAuthenticated = findViewById(R.id.changePasswordActivityTextViewUpdateEmailAuthenticated);
         changePasswordActivityProgressBar = findViewById(R.id.changePasswordActivityProgressBar);
 
@@ -55,6 +89,7 @@ public class ChangePasswordActivity extends AppCompatActivity {
         changePasswordActivityEditTextNewPassword.setEnabled(false);
         changePasswordActivityEditTextConfirmPassword.setEnabled(false);
         changePasswordActivityButtonConfirmPassword.setEnabled(false);
+        changePasswordActivityButtonCancel.setEnabled(false);
 
         authProfile = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = authProfile.getCurrentUser();
@@ -93,12 +128,19 @@ public class ChangePasswordActivity extends AppCompatActivity {
                                 changePasswordActivityProgressBar.setVisibility(View.GONE);
 
                                 //Disable editText for Current Password. Enable EditText for New Password and Confirm New Password
+                                changePasswordActivityEditTextCurrentPassword.setText("");
                                 changePasswordActivityEditTextCurrentPassword.setEnabled(false);
                                 changePasswordActivityEditTextNewPassword.setEnabled(true);
                                 changePasswordActivityEditTextConfirmPassword.setEnabled(true);
+                                changePasswordActivityButtonCancel.setEnabled(true);
 
                                 //Enable change password button and disable authenticate button
                                 changePasswordActivityButtonAuthenticate.setEnabled(false);
+                                if(!changePasswordActivityButtonAuthenticate.isEnabled()){
+                                    changesMadeAuthentication = true;
+                                } else {
+                                    changesMadeAuthentication = false;
+                                }
                                 changePasswordActivityButtonConfirmPassword.setEnabled(true);
 
                                 //Set TextView to show User is authenticated/verified
@@ -113,6 +155,12 @@ public class ChangePasswordActivity extends AppCompatActivity {
                                     @Override
                                     public void onClick(View v) {
                                         changePassword(firebaseUser);
+                                    }
+                                });
+                                changePasswordActivityButtonCancel.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        onBackPressed();
                                     }
                                 });
                             } else {
@@ -227,5 +275,42 @@ public class ChangePasswordActivity extends AppCompatActivity {
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://mail.google.com/"));
             startActivity(browserIntent);
         }
+    }
+
+    public void onBackPressed() {
+        if (changesMadeCurrentPassword == true || changesMadeAuthentication == true) {
+            // Changes have been made, show confirmation dialog
+            showExitConfirmationDialog();
+        }
+        else {
+            // No changes, perform default back action
+            Intent intent = new Intent(ChangePasswordActivity.this, UserUpdateProfileActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void showExitConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_exit, null);
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+        dialog.setCancelable(false);
+
+        TextView messageTextView = dialogView.findViewById(R.id.dialog_message);
+        Button yesButton = dialogView.findViewById(R.id.dialog_button_yes);
+        Button noButton = dialogView.findViewById(R.id.dialog_button_no);
+
+        yesButton.setOnClickListener(view -> {
+            // User confirmed, exit the activity
+            finish();
+        });
+
+        noButton.setOnClickListener(view -> {
+            // User canceled, dismiss the dialog
+            dialog.dismiss();
+        });
+
+        dialog.show();
     }
 }
